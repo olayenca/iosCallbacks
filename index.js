369694a6f48017d6a39bc437ccdc68aa97059d2c
Olayinka Otuniyi <o.otuniyi@elsevier.com>
2020-04-15 21:10:33 +0100

    extend-url (#4)
    * extend-url appends code
    
    * extend-url fix template
    
    * extend-url added template
    
    Co-authored-by: olayenca <olayinkaotuniyi@yahoo.co.uk>

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function backToApp () {
                 qr[p.split("=")[0]] = p.split("=")[1];
             });
         document.querySelector('body').innerHTML = `<div style='display:flex; justify-content:center;align-items:center;height:100%;'><button onclick='backToApp()'>Return to App</button></div>`;
-        window.location = new URL('appName://?code=${qr.code}');
+
+      window.location = new URL(`appName://?code=${qr.code}`);
         console.log("has code, callback triggered");
     }
 }
