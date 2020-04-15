7cc8cfb6625146a0eb571091770ddabe7f4191d2
Olayinka Otuniyi <o.otuniyi@elsevier.com>
2020-04-15 21:00:32 +0100

    Revert "extend-url appends code (#2)"
    This reverts commit a9a835b175e6c9cde3526d4095fae3f64cb93a1d.

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 function backToApp () {
     if (window.location.href.indexOf('code') > 0) {
-        const qr = {};
-        window.location.search
-            .substring(1)
-            .split("&")
-            .forEach(p => {
-                qr[p.split("=")[0]] = p.split("=")[1];
-            });
         document.querySelector('body').innerHTML = `<div style='display:flex; justify-content:center;align-items:center;height:100%;'><button onclick='backToApp()'>Return to App</button></div>`;
-        window.location = new URL('appName://?code=${qr.code}');
-        console.log("has code, callback triggered");
+        window.location = new URL('appName://');
     }
 }
