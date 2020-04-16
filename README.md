f3297a4d51d149aad17d119d80496565553849bf
olayenca <olayinkaotuniyi@yahoo.co.uk>
2020-04-16 20:43:57 +0100

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-#WIP
+# ios-app-webpage
+
+This webpage maps to a mobile [app](https://github.com/olayenca/xcode-url-scheme) via xcode URL Scheme after an oAuth validation.
+
+
+
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 function backToApp () {
     if (window.location.href.indexOf('code') > 0) {
-        const qr = {};
+        const parameters = {};
         window.location.search
             .substring(1)
-            .split("&")
-            .forEach(p => {
-                qr[p.split("=")[0]] = p.split("=")[1];
+            .split('&')
+            .forEach(param => {
+                parameters[param.split('=')[0]] = param.split('=')[1];
             });
-        document.querySelector('body').innerHTML = `<div style='display:flex; justify-content:center;align-items:center;height:100%;'><button onclick='backToApp()'>Return to App</button></div>`;
 
-      window.location = new URL(`appName://?code=${qr.code}`);
-        console.log("has code, callback triggered");
+        document.querySelector('body').innerHTML =
+            "<div style='display:flex;justify-content:center;align-items:center;height:100%;'><button onclick='backToApp()'>Return to App</button></div>";
+
+        window.location = new URL(`appName://?code=${parameters.code}`);
+        console.log('Page url has code parameter, callback triggered');
     }
 }
